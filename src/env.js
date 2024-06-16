e72b35eb5dc09d6b1fe9cf394b83e768f246bce5
Trevor Eyre <trevoreyre@gmail.com>
2024-06-16 01:51:08 -0400

    Added deploy button

--- src/env.js
+++ src/env.js
@@ -1,6 +1,7 @@
 const env = {
   googleMapsApiKey: import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY,
   googleMapsMapId: import.meta.env.PUBLIC_GOOGLE_MAPS_MAP_ID,
+  netlifyDeployUrl: import.meta.env.PUBLIC_NETLIFY_DEPLOY_URL,
 }
 
 export { env }
